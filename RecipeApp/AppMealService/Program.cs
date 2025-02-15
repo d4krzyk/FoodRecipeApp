89d46fa1566c85af4a8277a0831b4f54930e9006
Daniel Krzykowski <50460458+d4krzyk@users.noreply.github.com>
2025-02-15 19:28:47 +0100

    zmiany nazw

---
+++ RecipeApp/AppMealService/Program.cs
@@ -0,0 +1,52 @@
+﻿using AppLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMealService
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        static void Main()
+        {
+#if DEBUG
+            // Debugowanie w postaci aplikacji konsolowej
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new MealService()
+            };
+            ServiceBase.Run(ServicesToRun);
+            using (ServiceHost host = new ServiceHost(typeof(RecipeLibrary)))
+            {
+                host.Open();
+                Console.WriteLine("Usługa uruchomiona. Naciśnij [Enter], aby zakończyć...");
+                Console.ReadLine();
+            }
+            
+#else
+            // Normalne uruchomienie jako usługa Windows
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new MealService()
+            };
+            ServiceBase.Run(ServicesToRun);
+            using (ServiceHost host = new ServiceHost(typeof(RecipeLibrary)))
+            {
+                host.Open();
+                Console.WriteLine("Usługa uruchomiona. Naciśnij [Enter], aby zakończyć...");
+                Console.ReadLine();
+            }
+#endif
+        }
+    }
+}
